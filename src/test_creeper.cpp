3424231731a98c97e0a8f81ad444caae1692afbd
Alexey Kreshchuk <akrsch@gmail.com>
2022-02-01 18:39:40 +0300

    add channel to test_creeper

--- src/test_creeper.cpp
+++ src/test_creeper.cpp
@@ -1,3 +1,5 @@
+#include "BpskAwgnChannel.h"
+#include "Encoder.h"
 #include "PolarCode.h"
 #include "BaseDecoder.h"
 #include "ScCreeperDecoder.h"
@@ -6,9 +8,11 @@
 #include "ScFlipFanoDecoder.h"
 #include "ScStackDecoder.h"
 #include "ScStackOptimizedDecoder.h"
+#include <cassert>
 #include <iostream>
 #include <stdexcept>
 #include <tuple>
+#include <random>
 
 int main() {
     std::vector<int> usedBits = {47, 55, 59, 61, 62, 63, 79, 87, 91, 92, 93, 94, 95, 103, 106, 107, 108, 109, 110, 111, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 143, 150, 151, 153, 154, 155, 156, 157, 158, 159, 165, 166, 167, 169, 170, 171, 172, 173, 174, 175, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 195, 197, 198, 199, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255};
@@ -19,5 +23,17 @@ int main() {
     for (auto elem : result)
         std::cout << elem;
     std::cout << "\n";
+    Encoder encoder(&code);
+    std::vector<int> u(code.k());
+    std::uniform_int_distribution<int> d(0,1);
+    std::default_random_engine rng;
+    for (auto& x : u) x = d(rng);
+    auto c = encoder.Encode(u);
+    BpskAwgnChannel chan;
+    chan.SetSnr(10);
+    result = decoder.Decode(chan.Pass(c));
+    for (std::size_t i=0; i<result.size(); ++i)
+        assert(result[i] == u[i]);
+    // std::cout << "\n";
     return 0;
 }
